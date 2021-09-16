cb87b10048e0f6860f61cf7f3c15e4175fa2e819
chao <22640673+chao7150@users.noreply.github.com>
2021-09-17 03:37:30 +0900

    remove jquery

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 run()
 
 async function run() {
-  const tweetButton = document.querySelector("button.js-send-button");//jqueryオブジェクトにするとobserve出来ない
-  if (tweetButton === null) {
-      setTimeout(run, 500);
-      return;
+  const sendButton = document.querySelector(".js-send-button");
+  if (!sendButton) {
+    setTimeout(run, 500);
+    return;
   }
-  const tweetButtonContainer = $(".js-send-button-container")
-  const hashtagInput = $('<form><input id="js-hashtag-form"></input></form>',)
-  tweetButtonContainer.after(hashtagInput)
+  const hashtagInput = document.createElement("input");
+  hashtagInput.id = "js-hashtag-form";
+  hashtagInput.placeholder = "#hashtag";
+  document.querySelector(".js-account-safeguard-checkbox").after(hashtagInput);
+  const composeTextArea = document.querySelector(".js-compose-text");
 
-  $(document).keydown((e) => {
-    if (e.shiftKey && e.keyCode === 13) {
-      const tweetTextArea = $("textarea.js-compose-text")[0]
-      const hashtag = $("#js-hashtag-form").val()
-      tweetTextArea.value = tweetTextArea.value + " " + hashtag
-      tweetTextArea.dispatchEvent(new Event('change'))
-      $(".js-send-button")[0].dispatchEvent(new Event('click'))
+  document.addEventListener("keydown", (e) => {
+    if (e.shiftKey && e.key === "Enter") {
+      composeTextArea.value = `${composeTextArea.value} ${hashtagInput.value}`;
+      composeTextArea.dispatchEvent(new Event('change'));
+      sendButton.dispatchEvent(new Event("click"));
     }
   })
 }
